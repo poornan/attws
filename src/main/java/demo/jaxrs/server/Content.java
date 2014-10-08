6d78670f284e9c4c5dfb47104bdb3e4ae2056525
prindicka <prithy1209@gmail.com>
2014-10-08 10:50:18 +0530

    Content

---
+++ src/main/java/demo/jaxrs/server/Content.java
@@ -0,0 +1,29 @@
+package demo.jaxrs.server;
+import javax.xml.bind.annotation.*;
+
+/**
+ * Created by prindu on 08/10/14.
+ */
+@XmlType
+public class Content {
+
+    private long content_id;
+    private String level,presenter,reads;
+
+
+   public long getContent_id(){return content_id;}
+
+    public void setContent_id(long content_id){this.content_id=content_id;}
+
+  public String getLevel(){return  level;}
+    public  void setLevel(String level){this.level=level;}
+
+    public String getPresenter(){return presenter;}
+    public void setPresenter(String presenter){this.presenter=presenter;}
+
+    public String getReads(){return reads;}
+    public void setReads(String reads){this.reads=reads;}
+
+
+
+}
